ab0fd67695f36787eb1fefe92faee7b926d1f6e2
Adil Reza <adilreza043@gmail.com>
2019-04-13 23:31:03 +0600

    adding again

--- siliconvalleynest/settings.py
+++ siliconvalleynest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'na0+_rcp5v$3ezyd4ymand=5he(sv%pwqsd506oe35wvubj+!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
